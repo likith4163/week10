4ea7e3a560a507684e4c6e8b6847a756728641fa
likith4163 <aravapalli.likith@gmail.com>
2025-09-01 13:35:59 +1000

    m

--- frontend/main.js
+++ frontend/main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const PRODUCT_API_BASE_URL = '_PRODUCT_API_URL_';
     const ORDER_API_BASE_URL = '_ORDER_API_URL_';
 
-    // Product Service is named 'product-service-w04e2' and exposes port 8000 internally.
+    // Product Service is named 'product-service-w04e2' and exposes port 8000 internally.qqqqq
     //const PRODUCT_API_BASE_URL = 'http://product-service-w04e2:8000';
     // Order Service is named 'order-service-w04e2' and exposes port 8001 internally.
     //const ORDER_API_BASE_URL = 'http://order-service-w04e2:8001';
